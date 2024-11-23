c76b23dd7aff00700f085f0152da67845e0e6b3f
DavidSeyserGit <david.seyser@hotmail.com>
2024-11-23 11:01:51 +0100

    added motorcontrol

--- src/client.cpp
+++ src/client.cpp
@@ -4,28 +4,24 @@
 #include <chrono>
 #include "TCP.h"
 
-#define PORT 9997
+#define PORT 9999
 
 int main() {
     auto client = std::make_shared<TCP>(); // Create a shared TCP instance
 
     // Connect to server
-    if (!client->connectToServer("127.0.0.1", PORT)) {
+    if (!client->connectToServer("192.168.100.51", PORT)) {
         std::cerr << "Failed to connect to server." << std::endl;
         return 1;
     }
 
-    while (true) {
-        auto now = std::chrono::steady_clock::now();
-        auto time = std::chrono::duration_cast<std::chrono::seconds>(now.time_since_epoch()).count();
-
-        std::string message = "test " + std::to_string(time);
-        if (!client->sendData(message)) {
-            std::cerr << "Failed to send data. Exiting." << std::endl;
-            break;
-        }
-        std::this_thread::sleep_for(std::chrono::seconds(2)); //sleep
+    std::string message = "---START---{\"linear\": 0.0, \"angular\": 0}___END___";
+    if (!client->sendData(message)) {
+        std::cerr << "Failed to send data. Exiting." << std::endl;
+        return 1;
     }
+    std::cout << message << std::endl;
+    std::this_thread::sleep_for(std::chrono::seconds(2)); //sleep
 
     client->closeConnection();
     return 0;
