f5e761c8691df7a562a0bc0b1388e9dcb4c4b213
DavidSeyserGit <david.seyser@hotmail.com>
2024-11-23 16:08:56 +0100

    added folder structure and client file

---
+++ include/client/client.h
@@ -0,0 +1,14 @@
+#ifndef CLIENT_H
+#define CLIENT_H
+
+class Client {
+public:
+    Client();
+    ~Client();
+
+
+private:
+
+};
+
+#endif // CLIENT_H
---
+++ src/client.cpp
@@ -0,0 +1,5 @@
+#include "../include/client/client.h"
+
+int main(){
+    return 0;
+}
